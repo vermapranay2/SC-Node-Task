10f0a396244cbb05c7c842dcb1b85902710f3ecc
pranay <vermapranay2@gmail.com>
2019-02-05 19:19:56 +0530

    thumbnail generator added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var jwt = require('jsonwebtoken');
+var express = require('express')
+var jwt = require('jsonwebtoken')
 var bodyParser = require('body-parser')
 var jsonpatch = require('jsonpatch')
 var _ = require('lodash')
+var fs = require('fs')
+var request = require('request')
+var sharp = require('sharp')
 
 var app = express();
 
 app.use(bodyParser.json())
+
 
 app.post('/task/login', (req, res) => {
     // Mock user
@@ -54,7 +58,7 @@ app.post('/task/patch',verifyToken,  (req, res) => {
                 else{
                     res.json({
                         message: 'A field is missing'
-                      });
+                     });
                 }
             }
         });
@@ -67,24 +71,79 @@ app.post('/task/patch',verifyToken,  (req, res) => {
     }
     
     });
+app.post('/task/thumbnail',verifyToken, (req,res)=>{
+    
+    if(!_.isEmpty(req.body)){
+        url=req.body.URL
+        filename = _.last(url.split("/") )
+        jwt.verify(req.token, 'secretkey', (err, authData) => {
+            if(err) {
+              res.sendStatus(403);
+            } else {
+                if (url){
+                    request(url).pipe(fs.createWriteStream(filename)).on('close',()=>{
+            
+                        sharp(filename)
+                        .resize(50, 50)
+                        .toFile(filename+'resize.png', (err, info) => {
+                            if (err){
+                                res.json({
+                                    message:"image cannot be resized"
+                                });
+        
+                            }
+                            else{
+                                var img = fs.readFileSync(filename+'resize.png');
+                                res.writeHead(200, {'Content-Type': 'image/png' });
+                                res.end(img, 'binary');
+                            }
+                        } );
+                    
+                    
+                    
+        
+                } )
+            
+                }
+                else{
+                    res.json({
+                        message: 'A field is missing'
+                     });
+                }
+            }
+        });
+        
+        
+        
+        
+        }
+    else{
+        res.json({
+            message:"Body not provided"
+        });
+        }
 
-  function verifyToken(req, res, next) {
+    });
+
+    
+    function verifyToken(req, res, next) {
     // Get auth header value
-    const bearerHeader = req.headers['authorization'];
+        const bearerHeader = req.headers['authorization'];
     // Check if bearer is undefined
-    if(typeof bearerHeader !== 'undefined') {
+        if(typeof bearerHeader !== 'undefined') {
       // Split at the space
-      const bearer = bearerHeader.split(' ');
+            const bearer = bearerHeader.split(' ');
       // Get token from array
-      const bearerToken = bearer[1];
+            const bearerToken = bearer[1];
       // Set the token
-      req.token = bearerToken;
+            req.token = bearerToken;
       // Next middleware
-      next();
-    } else {
+            next();
+        } 
+        else {
       // Forbidden
-      res.sendStatus(403);
-    }
+            res.sendStatus(403);
+        }
   
   }
 
